8c6173dc5a944df9e6f83a5a6f6b9cbb8873066f
Uday Pratap Chauhan <96443357+udc29h@users.noreply.github.com>
2024-10-22 15:24:48 +0000

    initial commit

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <article className="footer">
+      <a
+        href="https://github.com/catherineisonline/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Designed & Built by Uday &copy;2025
+      </a>
+    </article>
+  );
+};
+
+export default Footer;
---
+++ src/routes/about/Languages.js
@@ -0,0 +1,15 @@
+const Languages = () => {
+  return (
+    <section className="languages">
+      <h2 className="title-font  pink-text h2-tag">Languages</h2>
+      <section>
+        <h3>English</h3>
+        <p>Native or bilingual proficiency</p>
+        <h3>Hindi</h3>
+        <p>Bilingual proficiency</p>
+      </section>
+    </section>
+  );
+};
+
+export default Languages;
